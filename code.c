29fff1b9c5496b8364939916f9f11ece3cd1b126
linususer <gamer-maroc@live.fr>
2021-10-30 17:04:24 -0400

    code

--- code.c
+++ code.c
@@ -1,8 +1,28 @@
+#include<unistd.h>
 #include<stdio.h>
 
-int main(void)
+int	ft_putchar(char c)
 {
-	printf("Hello world");
-	return 0;
+	write(1, &c, 1);
+	return (0);
+}
 
+void	ft_putnbr(int n)
+{
+	if(n >= 10)
+	{
+		ft_putnbr(n / 10);
+		ft_putnbr(n % 10);
+		remove()
+	}
+else
+	{
+		ft_putchar(n + '0');
+	}
+}
+
+int	main(void)
+{
+	ft_putnbr(100 );
+	return (0);
 }
